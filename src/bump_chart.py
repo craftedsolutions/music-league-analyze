7332b3e2b2994ed60bf0e2aadacc23d4ae74a6fa
jeremygustine <jeremy@crafted.solutions>
2023-03-28 03:39:44 -0600

    Fix bump chart script, but data problem remains

--- src/bump_chart.py
+++ src/bump_chart.py
@@ -9,21 +9,25 @@ df = pd.read_sql_query(query, cnx)
 
 df.set_index(['week', 'name', 'votes_in_week', 'running_score'])
 
-# transform df so that rows are weeks, columns are names, and cells are the ranking
-ranking_data = df.pivot(index='week', columns='name', values='rank')
+print(df.to_string())
+
+# transform df so that rows are weeks, columns are names, and cells are the rank
+ranking_data = df.pivot_table(index='week', columns='name', values='rank', aggfunc='first')
 
 # change data on 0th row to disambiguate the tie that looks funny
-ranking_data.at[0, 'Cole Capsalis'] = 1
-ranking_data.at[0, 'Chip M'] = 2
-ranking_data.at[0, 'kseniya'] = 3
-ranking_data.at[0, 'CharTheLatte'] = 4
-ranking_data.at[0, 'TJ Bowen'] = 5
-ranking_data.at[0, 'Jeremy Gustine'] = 6
-ranking_data.at[0, 'Aaron Knoll'] = 7
-ranking_data.at[0, 'Adam Oliver'] = 8
-ranking_data.at[0, 'tedpoatsy'] = 9
-ranking_data.at[0, 'rtrunck'] = 10
-ranking_data.at[0, 'Laura Poatsy'] = 11
+ranking_data.at[0, 'CharTheLatte'] = 1
+ranking_data.at[0, 'nat9172'] = 2
+ranking_data.at[0, 'Cody Palmer'] = 3
+ranking_data.at[0, 'jgracie6'] = 4
+ranking_data.at[0, 'rtrunck'] = 5
+ranking_data.at[0, 'Aaron Knoll'] = 6
+ranking_data.at[0, 'Adam Oliver'] = 7
+ranking_data.at[0, 'TJ Bowen'] = 8
+ranking_data.at[0, 'Cappy Hausfeld'] = 9
+ranking_data.at[0, 'Matt Markiewicz'] = 10
+ranking_data.at[0, 'Chip M'] = 11
+ranking_data.at[0, 'Jeremy Gustine'] = 12
+ranking_data.at[0, 'Cole Capsalis'] = 13
 
 print(ranking_data)
 
